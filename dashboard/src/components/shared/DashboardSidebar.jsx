47ab10753345d094e27b7c8892ab5f19bb260cd1
minhtrong456 <mtrong100@gmail.com>
2024-07-21 14:13:13 +0700

    change color of dashboard sidebar

--- dashboard/src/components/shared/DashboardSidebar.jsx
+++ dashboard/src/components/shared/DashboardSidebar.jsx
@@ -8,7 +8,7 @@ const DashboardSidebar = () => {
   const location = useLocation();
 
   return (
-    <div className="w-[300px] sticky top-0 left-0 border border-right h-screen p-3 shadow-sm flex-shrink-0 bg-white z-10">
+    <div className="w-[300px] sticky top-0 left-0  border-right h-screen p-3 shadow-sm flex-shrink-0 bg-blue-900 text-white z-10">
       <h1 className="text-center text-3xl font-bold flex items-center gap-2 justify-center">
         <FaHandHoldingMedical />
         Medical
@@ -25,11 +25,13 @@ const DashboardSidebar = () => {
               key={link.name}
               to={link.link}
               className={`${
-                activeLink ? "bg-blue-200" : "hover:bg-slate-200"
-              } flex cursor-pointer items-center gap-3 px-5 rounded-md h-[48px] transition-all`}
+                activeLink
+                  ? "bg-white/20 font-semibold"
+                  : "hover:bg-white/10 font-medium"
+              } flex cursor-pointer items-center gap-3 px-5 rounded-md h-[45px] transition-all`}
             >
               <i className={link.icon}></i>
-              <span className="font-medium">{link.name}</span>
+              <span>{link.name}</span>
             </Link>
           );
         })}
